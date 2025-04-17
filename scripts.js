26dd9ba10ddc49f695f838e45942f448a5f0986c
TomBirdi910 <dmitrienkodima20@gmail.com>
2025-04-17 23:06:30 +1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 function toggleMenu() {
     const menu = document.querySelector('.nav-menu');
-    menu.classList.toggle('active');
+    menu.style.display = (menu.style.display === 'block' ? 'none' : 'block');
 }
