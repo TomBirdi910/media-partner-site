45601ad647001d8baa3746d4cbcec82525aa86b4
TomBirdi910 <dmitrienkodima20@gmail.com>
2025-04-18 19:11:02 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const hamburgerMenu = document.getElementById('hamburger-menu');
 const navbar = document.getElementById('navbar');
 
 hamburgerMenu.addEventListener('click', () => {
-    navbar.classList.toggle('open');
+    navbar.classList.toggle('open');  // Для плавного открытия меню
     hamburgerMenu.classList.toggle('open');  // Для анимации гамбургер-меню и крестика
 });
